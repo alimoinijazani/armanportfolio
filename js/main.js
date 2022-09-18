a210a2baee7848415af75543c42c5578dcc5842a
alimoinijazani <96163151+alimoinijazani@users.noreply.github.com>
2022-09-18 12:38:15 +0430

    Update main.js

--- js/main.js
+++ js/main.js
@@ -67,73 +67,31 @@ tabs.forEach(tab=>{
 
 
 
-// modal
 
-// const modalViews=document.querySelectorAll('.services__modal')
-// const modalBtns=document.querySelectorAll('.services__button')
-// const modalCloses=document.querySelectorAll('.services__modal-close')
+// SWIPPER
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
 
-// let modal=function(modalClick){
-//     modalViews[modalClick].classList.add('active-modal')
-// }
-// modalBtns.forEach((modalBtn,i)=>{
-//     modalBtn.addEventListener('click',()=>{
-//         modal(i)
-//     })
-// })
-
-// modalCloses.forEach((modalClose)=>{
-//    modalClose.addEventListener('click',()=>{
-//     modalViews.forEach((modalView)=>{
-//         modalView.classList.remove('active-modal')
-//     })
-//    })
-// })
-
-
-// More
-
-function readMore() {
-    const dots = document.getElementById("dots");
-    const more = document.getElementById("more");
-    const myBtn = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      myBtn.innerHTML = "Read more";
-      more.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      myBtn.innerHTML = "Read less";
-      more.style.display = "inline";
-    }
-  } 
-
-
-
-myBtn.addEventListener('click',readMore)
-
-// scroll section active link
-const sections=document.querySelectorAll('section[id]')
-
-function scrollActive(){
-    const scrollY=window.pageYOffset
-
-    sections.forEach(current=>{
-        const sectionHeight=currentoffsetHeight
-        const sectionTop=current.offsetTop-50
-        let sectionId=current.getAttribute('id')
-    
-        if(scrollY>sectionTop && scrollY<=sectionTop+sectionHeight){
-            document.querySelector('.nav__menu a[href*='+sectionId+']').classList.add('active-link')
-        }
-        else{
-            document.querySelector('.nav__menu a[href*='+sectionId+']').classList.remove('active-link')
-        }
-
+  //READMORE
+  const parent=document.querySelectorAll('.container')
+  parent.forEach(elem=>{
+    elem.addEventListener('click',event=>{
+        const current=event.target
+        const isBtn=current.className.includes('readmore-btn')
+        if(!isBtn) return;
+        const currentText=current.parentNode.querySelector('.readmore')
+        currentText.classList.toggle('readmore-show')
+        current.textContent =
+        current.textContent === "read more" ? "read less" : "read more";
     })
-}
-
-window.addEventListener('scroll',scrollActive)
-
-// CHANGE BACKGROUND HEADER
+  })
